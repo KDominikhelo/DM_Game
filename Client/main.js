4402096ef559e7cca335cfbd1c8c57c1d8d9993e
martinkovacs22 <martinkovacs22@gmail.com>
2023-02-15 17:43:49 +0100

    sokváltozatás

--- Client/main.js
+++ Client/main.js
@@ -1,2 +1,12 @@
 
 var cnv = document.createElement('canvas');
+
+
+const update =async ()=>{
+
+await fetch("https://8080-cs-781916595840-default.cs-europe-west4-bhnf.cloudshell.dev/getdata",(err,res)=>{
+
+});
+
+    requestAnimationFrame(update);
+}
--- Server/index.js
+++ Server/index.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.route("getdata").get((req,res)=>{
+    res.send("hello");
+})
+
 
 app.listen(3000,console.log("fut"+3000));
